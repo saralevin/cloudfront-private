7d1f7e4506b7e1ea909c4fd3df98181185f094e1
saralevin <saralevin12@gmail.com>
2013-04-18 12:20:05 +0300

    Update base.rb
    add expire parameter to url

--- lib/cloudfront-private/streaming/base.rb
+++ lib/cloudfront-private/streaming/base.rb
@@ -15,25 +15,25 @@ module Cloudfront
 				include Cloudfront::Private::Configuration
 				
 				module ClassMethods
-				  def get_url(_request, _resource)
-				    url = get_signed_url(_resource, _request)
+				  def get_url(_request, _resource,expire)
+				    url = get_signed_url(_resource, _request,expire)
 				    # Escape characters for proper embedding in Flash parameters
 				    return url.gsub("?","%3F").gsub("=","%3D").gsub("&","%26")
 				  end
 
 				  protected				
-				    def get_signed_url(_resource, _request)
+				    def get_signed_url(_resource, _request,expire)
 				      pk_file        = Rails.root.join('certs',pem_file)
 				      key            = OpenSSL::PKey::RSA.new(File.readlines(pk_file).join(""))
-				      policy         = create_policy(_resource, _request)
+				      policy         = create_policy(_resource, _request,expire)
 				      sigcrypt       = key.sign(OpenSSL::Digest::SHA1.new, policy)
 				      urlsig         = escape_string(Base64.encode64(sigcrypt))
 				      encoded_policy = escape_string(Base64.encode64(policy))
 				      return "#{_resource}?Policy=#{encoded_policy}&Signature=#{urlsig}&Key-Pair-Id=#{key_pair_id}"
 				    end
 				
-				    def create_policy(_resource, _request)
-				      expires = 1.hour.from_now.to_i
+				    def create_policy(_resource, _request,expire)
+				      expires = expire.to_i || 1.hour.from_now.to_i 
 				      # Skip IP check for localhost (AWS:SourceIp does not work with localhost)
 				      if Rails.env.development?
 				        policy = %{ {"Statement":
